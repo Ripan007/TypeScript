f1089f41de3361fbd012ebda6b5c7af1454f2b66
ripan <reponmondal007@gmail.com>
2023-12-02 17:16:31 +0000

    Dec 2, 2023, 10:46 PM

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Todo from '../models/Todo';
-import TodoItem from './TodoItem';
+import Todo from '../models/Todo'
+import TodoItem from './TodoItem'
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <>
-            {props.items.map((item) => <TodoItem key={item.id} text={item.text} />)}
+            {props.items.map((item) => (
+                <TodoItem key={item.id} text={item.text} />
+            ))}
         </>
     )
 }
 
-export default Todos;
+export default Todos
