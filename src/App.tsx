4fd70ccb489b969af25fe7c73224b210d4330446
ripan <reponmondal007@gmail.com>
2023-12-02 17:18:19 +0000

    Dec 2, 2023, 10:48 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Todos from "./components/Todos"
-import Todo from "./models/Todo"
-
+import Todos from './components/Todos'
+import Todo from './models/Todo'
 
 function App() {
-
   const todos = [new Todo('learn react'), new Todo('typeScript is awesome')]
-  return <>
-    <Todos items={todos} />
-  </>
+  return (
+    <>
+      <Todos items={todos} />
+    </>
+  )
 }
 
 export default App
