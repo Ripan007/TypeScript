5ba1da929562c4487bf88f0c25983cd9549cb253
ripan <reponmondal007@gmail.com>
2023-12-02 15:49:55 +0000

    Dec 2, 2023, 9:19 PM

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   console.log(add(10, 20));
 
   let numbers: Array<number> = [1, 3, 5]
+
+  let numbers1: number[] = [1, 2, 5]
+  console.log(numbers1);
   console.log(numbers);
 
   return <>
