ae622416b68332c688095c8a51ed58a0e9a26e5a
ripan <reponmondal007@gmail.com>
2023-12-02 16:18:39 +0000

    Dec 2, 2023, 9:48 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
+import Todos from "./components/Todos";
+
 function App() {
-  function add(a: number, b: number) {
-    return a + b
-  }
-  console.log(add(10, 20));
-
-  let numbers: Array<number> = [1, 3, 5]
-
-  let numbers1: number[] = [1, 2, 5]
-  console.log(numbers1);
-  console.log(numbers);
-
   return <>
-    welcome back</>
+    <Todos />
+  </>
 }
 
 export default App
---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+function Todos() {
+    return (
+        <ul>
+            <li>learning react</li>
+            <li>learning typescript</li>
+        </ul>
+    )
+
+}
+
+export default Todos;
